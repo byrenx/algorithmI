6e5e24036b185474361237e76ae029142f5139e4
Wei Wang <thomas.wei.wang@gmail.com>
2013-03-02 15:42:00 -0500

    added stack version (non-recursive) DFS

--- StrongConnectedComponents/scc.r
+++ StrongConnectedComponents/scc.r
@@ -21,11 +21,7 @@ BFS <- function(g, s){
 
 ## Connecdted-Component for Undirected Graph
 
-## Depth-First Search
-## because of recursion, we need to keep a global variable explored
-verteces <- unique(test1$V1)
-  explored <- rep(0, length(verteces))
-  names(explored) <- verteces
+## Depth-First Search via recursion
 DFS <- function(g, s){
   explored[as.character(s)] <<- 1
   W <- subset(g, V1==s)$V2
@@ -35,7 +31,10 @@ DFS <- function(g, s){
     }
   }
 }
-DFS(test1, 4)
+verteces <- unique(c(test$V1, test$V2))
+explored <- rep(0, length(verteces))
+names(explored) <- verteces
+DFS(test, 5)
 
 DFS_container <- function(g, s){
   verteces <- unique(test1$V1)
@@ -45,7 +44,7 @@ DFS_container <- function(g, s){
   return(explored)
 }
 
-## Strongly Connected Component based on DFS
+## Strongly Connected Component based on DFS_recursion
 ## below it is assumed that the nodes are indexed
 ## by 1:n
 DFS_1st <- function(g, i){
@@ -88,7 +87,93 @@ for (i in order(finish_time, decreasing = T)) {
 }
 table(leaders)
 
-## test1 <- read.table("test1.txt")
-## test2 <- read.table("test2.txt")
-## test3 <- read.table("test3.txt")
+## test <- read.table("test1.txt")
+## test <- read.table("test2.txt")
+## test <- read.table("test3.txt")
 ## test <- read.table("SCC.txt")
+
+
+## implement DFS via stack
+DFS_stack <- function(g, s){
+  verteces <- sort( unique(c(g$V1, g$V2)))
+  explored <- rep(0, length(verteces))
+##   names(explored) <- verteces
+  Q <- list(s)
+  while(length(Q) > 0){
+    v <- Q[[length(Q)]]
+    Q <- Q[-length(Q)]
+    if(explored[v]==0) {
+      explored[v] <- 1
+      W <- unique(subset(g, V1==v)$V2)
+      for (w in W) {
+        if (explored[w]==0) {
+          Q <- c(Q, w)
+        }
+      }
+    } 
+  }
+  return(explored)
+}
+
+## Strongly Connected Component based on DFS_stack
+## below it is assumed that the nodes are indexed
+## by 1:n
+DFS_stack_1st <- function(g, i){
+  Q <- list(i)
+  while(length(Q) > 0){
+    v <- Q[[length(Q)]]
+    Q <- Q[-length(Q)]
+    if(!explored[v]) {
+      explored[v] <<- 1      
+      t <<- t + 1
+      finish_time[v] <<- t
+      W <- unique(subset(g, V1==v)$V2)
+      for (w in W) {
+        if (!explored[w]) {
+          Q <- c(Q, w);
+          flag <- 1;
+        }
+      }
+    } 
+  }
+}
+
+DFS_stack_2nd <- function(g, i){
+  Q <- list(i)
+  while(length(Q) > 0){
+    v <- Q[[length(Q)]]
+    Q <- Q[-length(Q)]
+    if(!leaders[v]) {
+      leaders[v] <<- s
+      W <- unique(subset(g, V1==v)$V2)
+      for (w in W) {
+        if (!leaders[w]) 
+          Q <- c(Q, w)        
+      }      
+    } 
+  }
+}
+
+
+reversed <- function(g){ return(data.frame(V1=g$V2, V2=g$V1)) }
+
+verteces <- unique(sort(c(test$V1, test$V2)))
+finish_time <- rep(0, length(verteces))
+explored <- rep(0, length(verteces))
+t <- 0;
+for (i in rev(verteces)) {
+  if(!explored[i]) {
+    ## s <- i;
+    DFS_stack_1st(reversed(test), i)
+  }
+}
+
+leaders <- rep(0, length(verteces))
+s <- NULL
+for (i in order(finish_time, decreasing = T)) {
+  if(!leaders[i]) {
+    s <- i
+    DFS_stack_2nd(test, i)
+  }
+}
+head(sort(table(leaders), decreasing = T))
